814a9b4642a896cca32b12f7efb22fe286c4011f
tonnnyK <tonnnyK@gmail.com>
2018-01-14 16:55:33 +1100

    put creating and clearing grid into separate functions

--- sketch.js
+++ sketch.js
@@ -1,41 +1,49 @@
-const DIMENSION = 16;
-const container = document.querySelector('#gridContainer');
+let DIMENSION = 16;
+
+createGrid();
+clearGrid();
 
 /*
  * Create DIMENSION by DIMENSION grid.
  * #gridContainer will have DIMENSION rows,
  * each row will have DIMENSION cells.
  */
-for (let i = 0; i < DIMENSION; ++i) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    container.appendChild(row);
+function createGrid() {
+    for (let i = 0; i < DIMENSION; ++i) {
+        const container = document.querySelector('#gridContainer');
+        const row = document.createElement('div');
 
-    for (let j = 0; j < DIMENSION; ++j) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
+        row.classList.add('row');
+        container.appendChild(row);
 
-        row.appendChild(cell);
-        paint(cell);
+        for (let j = 0; j < DIMENSION; ++j) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+
+            row.appendChild(cell);
+            paint(cell);
+        }
     }
 }
 
 /*
  * Clears the grid
  */
-const clearButton = document.querySelector('#clearButton');
-clearButton.addEventListener('click', () => {
-    const container = document.querySelector('#gridContainer');
-    let rowList = container.childNodes;
+function clearGrid() {
+    const clearButton = document.querySelector('#clearButton');
+    clearButton.addEventListener('click', () => {
+        const container = document.querySelector('#gridContainer');
+        let rowList = container.childNodes;
 
-    for (let i = 0; i < DIMENSION; ++i) {
-        let cellList = rowList[i].childNodes;
+        for (let i = 0; i < DIMENSION; ++i) {
+            let cellList = rowList[i].childNodes;
 
-        for (let j = 0; j < rowList.length; ++j) {
-            cellList[j].style.backgroundColor = '#F9FAFB';
+            for (let j = 0; j < rowList.length; ++j) {
+                cellList[j].style.backgroundColor = '#F9FAFB';
+            }
         }
-    }
-});
+    });
+}
 
 /*
  * Event handler that changes colour of cell
